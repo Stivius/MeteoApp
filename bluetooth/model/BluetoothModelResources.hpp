7201152399b3bacd8c335baf2a07956222c1cd31
Valentyn Korniienko <kornienko-vr@rambler.ru>
2019-04-24 23:40:22 +0300

    Changed nested namespaces and replace few messages in Bluetooth resources

--- bluetooth/model/BluetoothModelResources.hpp
+++ bluetooth/model/BluetoothModelResources.hpp
@@ -2,34 +2,28 @@
 #include <QString>
 
 
-namespace Resources {
-
-    namespace BluetoothMessages
-    {
-        namespace Errors
-        {
-            const QString BLuetoothAdaptorIsPoweredOff  = "The Bluetooth adaptor is powered off.";
-            const QString IoError                       = "Writing or reading from the device resulted in an error.";
-            const QString UnknownError                  = "An unknown error has occurred.";
-            const QString CantFoundAnyBleDevices        = "No Low Energy devices found.";
-            const QString CantConnectToDevice           = "Cannot connect to remote device.";
-            const QString BleControllerDisconnected     = "LowEnergy controller disconnected";
-            const QString CantFindWeatherService        = "Weather service not found.";
-            const QString WrPropertyNotFound            = "WR Property not found.";
-            const QString InvalidDeviceDescrpitor       = "Invalid CC2540 descriptor";
-            const QString InvalidCharacteristic         = "Invalid notification/indication for read characteristic";
-        }
-        namespace Info
-        {
-            const QString ScanningMoreDevices           = "Low Energy device found. Scanning more..." ;
-            const QString ScanningDone                  = "Scanning done.";
-            const QString ScanningForDevices            = "Scanning for devices...";
-            const QString ConnectedTo                   = "Connected to: %1";
-            const QString SearchServices                = "Controller connected. Search services...";
-            const QString ServiceDiscovered             =  "Weather station discovered. Waiting for service scan to be done..." ;
-            const QString ScanHasDone                   = "Service scan has done.";
-            const QString DiscoveringServices           = "Discovering services...";
-            const QString ServiceDiscoveredState        = "Service discovered.";
-        }
-    }
+namespace Resources :: BluetoothMessages::Errors
+{
+    const QString BLuetoothAdaptorIsPoweredOff  = "Bluetooth adaptor is turned off";
+    const QString IoError                       = "Device reading/writing failed ";
+    const QString UnknownError                  = "An unknown error has occurred.";
+    const QString CantFoundAnyBleDevices        = "No Low Energy devices found.";
+    const QString CantConnectToDevice           = "Cannot connect to remote device.";
+    const QString BleControllerDisconnected     = "LowEnergy controller disconnected";
+    const QString CantFindWeatherService        = "Weather service not found.";
+    const QString WrPropertyNotFound            = "WR Property not found.";
+    const QString InvalidDeviceDescrpitor       = "Invalid CC2540 descriptor";
+    const QString InvalidCharacteristic         = "Invalid notification/indication for read characteristic";
+}
+namespace Resources :: BluetoothMessages::Info
+{
+    const QString ScanningMoreDevices           = "Low Energy device found. Scanning more..." ;
+    const QString ScanningDone                  = "Scanning done.";
+    const QString ScanningForDevices            = "Scanning for devices...";
+    const QString ConnectedTo                   = "Connected to: %1";
+    const QString SearchServices                = "Controller connected. Search services...";
+    const QString ServiceDiscovered             =  "Weather station discovered. Waiting for service scan to be done..." ;
+    const QString ScanHasDone                   = "Service scan has done.";
+    const QString DiscoveringServices           = "Discovering services...";
+    const QString ServiceDiscoveredState        = "Service discovered.";
 }
